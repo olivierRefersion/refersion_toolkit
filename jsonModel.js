e360b71d49c4fe38026bd382ccf1a6cfb2c4d7e1
Olivier <olivier@Oliviers-MacBook-Pro.local>
2022-04-07 09:18:53 -0400

    new project upload

---
+++ jsonModel.js
@@ -0,0 +1,27 @@
+{
+     {
+         "Title":"Bulk Manual Credit (Orders)",
+         "Description":"Use this tool to add bulk credit of orders to affiliates within an accountgit . ",
+         "Action":"function(formParam, cb){ yada make call await use callback for rendring results}",
+         "Response":"What Happens After/Error Handling", //ClassComponent
+         "URLTemplateDownload":"URL?"
+
+     },
+     {
+        "Title":"Bulk Manual Credit (Funds)",
+        "Description":"Use this tool to add bulk credit in funds to affiliates within an account.",
+        "Action":"function(formParam, cb){ yada make call await use callback for rendring results}",
+        "Response":"What Happens After/Error Handling", //ClassComponent
+        "URLTemplateDownload":"URL?"
+
+    },
+    {
+        "Title":"Webhook Orders",
+        "Description":"Use this tool to send in bulk orders via Webhook to an account.",
+        "Action":"function(formParam, cb){ yada make call await use callback for rendring results}",
+        "Response":"What Happens After/Error Handling", //ClassComponent
+        "URLTemplateDownload":"URL?"
+
+    }
+}
+
