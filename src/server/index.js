1b9bbd497c12bc87b7311a3d260592eb6d3ec16e
Olivier <olivier@ip-192-168-1-158.ec2.internal>
2022-04-18 00:56:48 -0400

    More code cleanup. Will be starting a new branc for response handling

--- src/server/index.js
+++ src/server/index.js
@@ -20,86 +20,55 @@ const port = process.env.PORT || 5000;
 
 app.post("/test",  (req, res) => {
 
-    console.log(req.body.jsonObj);
-    // for (let i = 0; i < req.body.jsonDataFromcsv.length; i++) {
-    //     // console.log(jsonObj[0].Order_ID);
-    //     // console.log(jsonObj[0].Affiliate_ID);
-    //     // console.log(jsonObj[0].Status);
-    //     // console.log(jsonObj[0].Notes);
-    //     let order_id = jsonObj[i].order_id; 
-    //     let id =  jsonObj[i].affiliate_id;
-    //     let status = jsonObj[i].status;
-    //     let notes = jsonObj[i].notes;
+   
+    //Take the json object from the frontend request and loop through it
+    for (let i = 0; i < req.body.jsonObj.length; i++) {
 
-    //     console.log
+    
+        //Set new variables for the properties on each object to be passed into the data for the Axios call
+        let id = req.body.jsonObj[i].id; 
+        let commission =  req.body.jsonObj[i].commission;
+        let status = req.body.jsonObj[i].status;
+        let currency = req.body.jsonObj[i].currency;
+        let notes = req.body.jsonObj[i].notes;
+        
 
-    //     // axios({
-    //     //         method: 'post',
-    //     //         url: '//api.refersion.com/v2/conversion/manual_credit',
-    //     //         headers: {
-    //     //             "Refersion-Public-Key": req.body.pubkey,
-    //     //             "Refersion-Secret-Key": req.body.secKey,
-    //     //             "Content-Type": "application/json",
-    //     //             "Access-Control-Allow-Origin": "*"
-    //     //         },
-    //     //         data: {
-    //     //             "id":"6498741",
-    //     //             "commission":1,
-    //     //             "status":"PENDING",
-    //     //             "currency":"USD",
-    //     //             "notes":"Testing"
-    //     //         }   
-    //     // })
-    //     // .then(function (response) {
-    //     // // handle success
-    //     //     console.log(`For ${order_id} the Post is ${response.statusText}. ${response.data.message}`);
-    //     // })
-    //     // .catch(function (error) {
-    //     //     console.log(`For ${order_id}, ${error.response.data.error}`);
-    //     //     // console.log(error);
-    //     // });
+        axios({
+                method: 'post',
+                url: 'https://api.refersion.com/v2/conversion/manual_credit',
+                headers: {
+                    "Refersion-Public-Key": req.body.pubKey,
+                    "Refersion-Secret-Key": req.body.secKey,
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                },
+                data: {
+                    "id": id,
+                    "commission": commission,
+                    "status": status,
+                    "currency":currency,
+                    "notes": notes
+                }   
+        })
+        .then(function (response) {
+        
+
+        //For reference in sending back to the front end results tables
+            console.log("Response status is" + response.status);
+            console.log("Response status text is" + response.statusText);
+            console.log("Response conversion ID is" + response.data.conversion_id);
+            console.log("Response Affiliate ID" + response.config.data.id);
+            console.log("Response Affiliate ID" + response.config.data.commmission);
+            console.log("Response Affiliate ID" + response.config.data.currency);
+        })
+        .catch(function (error) {
+            console.log(error);
+            // console.log(error);
+        });
             
 
 
-    // }
-    
-    
-    
-    
-    
-    
-    //TODO: Put this in a loop
-    //
-    //   axios({
-    //     method: 'post',
-    //     url: '//api.refersion.com/v2/conversion/manual_credit',
-    //     headers: {
-    //         "Refersion-Public-Key": req.body.pubkey,
-    //         "Refersion-Secret-Key": req.body.secKey,
-    //         "Content-Type": "application/json",
-    //         "Access-Control-Allow-Origin": "*"
-    //     },
-    //     data: {
-    //         "id":"6498741",
-    //         "commission":1,
-    //         "status":"PENDING",
-    //         "currency":"USD",
-    //         "notes":"Testing"
-    //     }   
-    //     })
-    //     .then(function (response) {
-    //     // handle success
-    //     console.log(response);
-    //     })
-    //     .catch(function (error) {
-
-    //         console.log(error);
-    //         // console.log(error);
-    //     });
-    
-    // console.log(req.body.pubKey);
-    // console.log(req.body.secKey);
-    // console.log(req.body.jsonDataFromcsv);
+    }
     res.json();
     
 })
